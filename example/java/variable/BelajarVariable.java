a9e75ebde4e14cdba0e9584ae350da21e7faefec
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-19 11:24:55 +0700

    membuat variable

---
+++ example/java/variable/BelajarVariable.java
@@ -0,0 +1,10 @@
+package example.java.variable;
+
+class BelajarVariable{
+
+    public static void main(String[] args){
+        String variableTipeDataKarakter = "Halo nama saya dimas maryanto";
+        System.out.println(variableTipeDataKarakter);
+    }
+
+}
