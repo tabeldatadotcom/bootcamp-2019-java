5289775254127b8efa050a3d3fc8fdb209d4ec9a
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-19 11:43:44 +0700

    update membuat tipe data number

---
+++ example/java/variable/BelajarVariableNumber.java
@@ -0,0 +1,18 @@
+package example.java.variable;
+
+public class BelajarVariableNumber{
+
+    public static void main(String[] args) {
+        Integer var1 = 1000;
+        Short ram = 127;
+        Double pecahanDouble = 10000.5;
+        Float pecahanFloat = 10.0f;
+        Long rupiah = 100_000_000l;
+        System.out.println("nilai berikut: ");
+        System.out.println("var1 = "+ var1);
+        System.out.println("ram = "+ ram);
+        System.out.println("pecahanDouble = "+ pecahanDouble);
+        System.out.println("pecahanFloat = "+ pecahanFloat);
+        System.out.println("rupiah = "+ rupiah);
+    }
+}
