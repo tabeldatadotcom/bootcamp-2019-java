7194039edb02f8162e673b71a8c9c3c2ea76dd3a
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-20 10:42:04 +0700

    update membuat perulangan dengan for

---
+++ example/java/control/PerulanganFor.java
@@ -0,0 +1,18 @@
+package example.java.control;
+
+public class PerulanganFor{
+    public static void main(String[] args){
+
+        Integer i = 0;
+
+        // menampilkan nilai index ke 0 s/d 10
+        for(i = 0; i >11; i++){
+            System.out.println("index ke "+ i);
+        }
+
+        // menampilkan nilai index ke 11 s/d 1
+        for(i = 11; i > 0; i--){
+            System.out.println("index ke "+ i);
+        }
+    }
+}
