b797f72ddc0223a38919caad753b1e5527ca64ce
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-02-21 16:33:32 +0700

    multiple object

--- example/java/oop/MainApplication.java
+++ example/java/oop/MainApplication.java
@@ -4,14 +4,17 @@ public class MainApplication{
 
     public static void main(String[] args){
         Sepeda polygon = new Sepeda();
-        System.out.println("jumlah roda : "+ polygon.getJumlahRoda()+ " berkecepatan: "+ polygon.kecepatanSekarang()+ " km/h");
+        System.out.println("polygon jumlah roda : "+ polygon.getJumlahRoda()+ " berkecepatan: "+ polygon.kecepatanSekarang()+ " km/h");
 
         polygon.meningkatkanSpeed(10);
-        System.out.println("berkecepatan: "+ polygon.kecepatanSekarang()+ " km/h");
+        System.out.println("polygon berkecepatan: "+ polygon.kecepatanSekarang()+ " km/h");
 
-        polygon = new Sepeda();
-        System.out.println("jumlah roda : "+ polygon.getJumlahRoda()+ " berkecepatan: "+ polygon.kecepatanSekarang()+ " km/h");
+        Sepeda asdkfld = new Sepeda();
+        System.out.println("asdkfld jumlah roda : "+ asdkfld.getJumlahRoda()+ " berkecepatan: "+ asdkfld.kecepatanSekarang()+ " km/h");
         
-        System.out.println("berkecepatan: "+ polygon.kecepatanSekarang()+ " km/h");
+        System.out.println("asdkfld berkecepatan: "+ asdkfld.kecepatanSekarang()+ " km/h");
+
+        polygon.meningkatkanSpeed(10);
+        System.out.println("polygon berkecepatan: "+ polygon.kecepatanSekarang()+ " km/h");   
     }
 }
